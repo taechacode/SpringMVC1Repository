5ebd88671fb48a43fcf65d0af89bc7ee07a10115
taechacode <kangtaechan2015@gmail.com>
2022-06-21 15:02:38 +0900

    Create repository

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloService.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        
+        // 쿼리 파라미터 ?username=kim 조회해보기
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // http body에 메시지를 담기
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+
+    }
+
+}
