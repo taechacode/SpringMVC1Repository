17af726ab40e0d51a99396fc176c8fe223df7c94
taechacode <kangtaechan2015@gmail.com>
2022-07-08 22:10:07 +0900

    Create hello.servlet.web.springmvc v1 And * v2 42강 스프링 MVC - 컨트롤러 통합까지의 예제 코드 작성

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,12 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +15,22 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+	
+	/*
+	application.properties에 있는 ViewResolver를 구현한 것
+	InternalResourceViewResolver는 JSP를 처리할 수 있는 뷰를 반환한다.
+	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}
+	 */
+
+	// @Component를 쓰지 않고 스프링 빈 직접 등록
+	/*
+	@Bean
+	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+		return new SpringMemberFormControllerV1();
+	}
+	*/
 
 }
