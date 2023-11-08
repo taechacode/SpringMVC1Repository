2826933bceaea1b40b70b62f0d782b9c1685741e
taechacode <kangtaechan2015@gmail.com>
2023-11-08 21:04:45 +0900

    Section 6 Review : HTTP 요청 메시지 - 단순 텍스트

--- springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -38,6 +38,20 @@ public class RequestBodyStringController {
         responseWriter.write("ok");
     }
 
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * - 메시지 바디 정보를 직접 조회 (@RequestParam X, @ModelAttribute X)
+     * -> 요청 파라미터(Get에 쿼리 파라미터 혹은 Post 방식으로 HTML Form 전달)를 조회하는 방식과 관계 없음.
+     * (요청 파라미터의 경우에는 @RequestParam과 @ModelAttribute 사용)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 응답에서도 HttpEntity 사용 가능
+     * - 메시지 바디 정보 직접 반환 (view 조회 X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 스프링MVC 내부에서 HTTP 메시지 바디를 읽어서 문자나 객체로 변환해서 전달해주는데,
+     * 이때 HTTP 메시지 컨버터('HttpMessageConverter')라는 기능을 사용한다.
+     */
     @PostMapping("/request-body-string-v3")
     public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
         String messageBody = httpEntity.getBody();
@@ -46,8 +60,10 @@ public class RequestBodyStringController {
         return new HttpEntity<>("ok");
     }
 
-    @ResponseBody
+    @ResponseBody // @ResponseBody를 사용하면 응답 결과를 HTTP 메시지 바디에 직접 담아서 전달할 수 있다. (view 사용 X)
     @PostMapping("/request-body-string-v4")
+    // @RequestBody를 사용하면 HTTP 메시지 바디 정보를 편리하게 조회 가능.
+    // 헤더 정보가 필요하다면 'HttpEntity'를 사용하거나 '@RequestHeader'를 사용하면 된다.
     public String requestBodyStringV4(@RequestBody String messageBody) {
         log.info("messageBody={}", messageBody);
         return "ok";
